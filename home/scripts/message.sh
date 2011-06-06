09457b408c276ec93bfe09499985cdfacbac10bc
benanderson.us@gmail.com <benanderson.us@gmail.com@0843c430-c97b-11dd-94fd-9de6169c3690>
2011-06-06 13:13:13 +0000

    no real changes - just moving stuff around

--- home/scripts/message.sh
+++ home/scripts/message.sh
@@ -1,5 +1,6 @@
 export CLASSPATH=/cygdrive/c/dev/java/rhino1_7R2/js.jar:/cygdrive/c/dev/java/smack_3_1_0/smack.jar;
+#export CLASSPATH="c:/dev/java/rhino1_7R2/js.jar;c:/dev/java/smack_3_1_0/smack.jar;$CLASSPATH"
 my_pwd=$PWD
 cd ~/scripts
-java -classpath `cygpath -wp $CLASSPATH` org.mozilla.javascript.tools.shell.Main send_msg.js "benanderson.us@gmail.com" "password" "benanderson@benanderson.us" "ping" 1> /dev/null 2> /dev/null
+java -classpath `cygpath -wp $CLASSPATH` org.mozilla.javascript.tools.shell.Main send_msg.js "benanderson.message@gmail.com" message99 "benanderson@benanderson.us" "ping" 1> /dev/null 2> /dev/null
 cd $my_pwd
