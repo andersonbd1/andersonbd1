c2811e87d73320b4856d2db04a97ce1022dbd0d4
benanderson.us@gmail.com <benanderson.us@gmail.com@0843c430-c97b-11dd-94fd-9de6169c3690>
2013-02-19 04:05:21 +0000

    most of the commands are working - lots of progress

--- trunk/cp/java/src/com/catholicpatrimony/GDocTransformerGr.groovy
+++ trunk/cp/java/src/com/catholicpatrimony/GDocTransformerGr.groovy
@@ -1,6 +1,5 @@
-import groovyx.net.http.*
-import static groovyx.net.http.ContentType.*
-import static groovyx.net.http.Method.*
+import groovyx.net.http.*; import static groovyx.net.http.ContentType.*;
+import static groovyx.net.http.Method.*;
 import au.com.bytecode.opencsv.CSVParser;
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.Template;
@@ -9,6 +8,27 @@ import org.apache.velocity.VelocityContext;
 
 import org.apache.commons.lang.StringUtils;
 
+// steps to setup a new class
+//   manually backup audio files and docs on s3
+//     s3cmd cp -r s3://tedesche/new_mass_translation/audio s3://tedesche/new_mass_translation/orig
+//     s3cmd cp -r s3://tedesche/new_mass_translation/docs s3://tedesche/new_mass_translation/orig
+//   manually get orig files
+//     s3cmd sync --exclude=orig s3://tedesche/new_mass_translation ./orig
+//   run this script with options: audio, zip, wp, podcast
+//   upload to s3
+//     s3cmd sync ./build/new_mass_translation/ s3://tedesche/new_mass_translation/generated/
+//   change perms for s3 docs
+//     ????
+//   upload to cp.com
+//     scp -i ~/.ssh/tedesche.pem -r ./build/web/* bitnami@catholicpatrimony.com:~/stack/apache2/htdocs/cp
+
+def ops = []
+ops.add("print");
+//ops.add("audio");
+//ops.add("zip");
+ops.add("wp");
+ops.add("podcast");
+
 def http = new HTTPBuilder( 'https://docs.google.com')
 
 /*
@@ -16,6 +36,7 @@ https://docs.google.com/spreadsheet/pub?key=0AkWmZX8HtwWHdENUNFcxdG9XdzBTaWhlVkZ
 https://docs.google.com/spreadsheet/pub?key=0AkWmZX8HtwWHdENUNFcxdG9XdzBTaWhlVkZ0RU1QcXc&output=csv&single=true&gid=1
 */
 
+//for (gid in 0..1) {
 for (gid in 0..1) {
   println 'gid: '+gid;
   def responseStr = null;
@@ -32,6 +53,7 @@ for (gid in 0..1) {
       println "My response handler got response: ${resp.statusLine}"
       println "Response length: ${resp.headers.'Content-Length'}"
       responseStr = reader.getText() // print response reader
+      println responseStr
     }
    
     // called only for a 404 (not found) status code:
@@ -40,10 +62,6 @@ for (gid in 0..1) {
     }
 
   }
-  println '2'
-
-  println responseStr.getClass();
-
   seriesLabels = [];
   seriesData = [:];
   classLabels = [];
@@ -62,6 +80,9 @@ for (gid in 0..1) {
       } else if (lineNumber == 2) {
         classLabels[i] = p;
       } else if (lineNumber >= 3) {
+        if (classLabels[i].equals('id') && p.length() == 1) {
+          p = '0'+p;
+        }
         def row = classes[lineNumber - 3]
         if (row == null) {
           row = [:]
@@ -80,21 +101,142 @@ for (gid in 0..1) {
       }
     };
   }
-  println classLabels;
-  println classes;
 
   def format = 'wp';
-  def writer = new BufferedWriter(new FileWriter("velocity_out/${seriesData.normalized_name}-${format}.xml"))
-  Template template = Velocity.getTemplate("velocity/${format}.vm");
-  Context context = new VelocityContext();
+  "mkdir -p ./build/web/${seriesData.normalized_name}".execute().waitFor();
+  "mkdir -p ./build/${seriesData.normalized_name}".execute().waitFor();
 
-  context.put("seriesData", seriesData);
-  context.put("classLabels", classLabels);
-  context.put("classes", classes);
+  for (c in classes) {
+    if (c.audio) {
+      c['newAudio'] = c.id +"-"+c.title.replaceAll(' ', '_')+'.mp3'
+    }
+    if (c.handout_file) {
+      if (c.handout_file instanceof Collection) {
+        c.new_handout_file = [];
+        for (def i=0; i<c.handout_file.size; i++) {
+          c.new_handout_file[i] = getNewHandoutFileName(c.id, c.handout_title[i], c.handout_file[i]);
+        }
+      } else {
+        c['new_handout_file'] = getNewHandoutFileName(c.id, c.handout_title, c.handout_file);
+      }
+    }
+  }
 
+  if (ops.contains('wp')) {
+    runVelocity(
+      "velocity/${format}.vm", 
+      "build/web/${seriesData.normalized_name}/${format}.php",
+      ["seriesData": seriesData, "classLabels": classLabels, "classes": classes]
+    );
+  }
+
+  if (ops.contains('print')) {
+    println classLabels;
+    println classes;
+  }
+
+  if (ops.contains('audio')) {
+    "rm -fR ./build/${seriesData.normalized_name}/audio".execute().waitFor();
+    "mkdir -p ./build/${seriesData.normalized_name}/audio".execute().waitFor();
+    if (!new File("orig/${seriesData.normalized_name}").exists())
+    {
+      def proc = "s3cmd sync s3://tedesche/${seriesData.normalized_name} ./orig".execute();
+      proc.waitFor();
+    }
+    for (c in classes) {
+      if (c.audio) {
+        def origFile = "orig/${seriesData.normalized_name}/audio/${c.audio}"
+        def newFile = "build/${seriesData.normalized_name}/audio/${c.newAudio}"
+        println c.audio;
+        proc("sox ${origFile} -r 24k -c 1 ${newFile}");
+        proc(["id3v2", "-a", "David Tedesche", "-A", seriesData.normalized_name, "-t", c.title, "-T", c.id, newFile]);
+      }
+    }
+  }
+
+  if (ops.contains('podcast')) {
+    for (c in classes) {
+      if (c.audio) {
+        def newFile = "build/${seriesData.normalized_name}/audio/${c.newAudio}"
+        def outStr = proc("soxi ${newFile}");
+        def matcher = outStr =~ /Duration *: (.*) =/
+        c["duration"] = matcher[0][1];
+        c["length"] = proc("ls -lad ${newFile}").split(" ")[4]
+        c["link2mp3"] = "http://tedesche.s3.amazonaws.com/${seriesData.normalized_name}/generated/audio/${c.newAudio}"
+      }
+    }
+    runVelocity(
+      "velocity/podcast.vm", 
+      "build/web/${seriesData.normalized_name}/podcast.xml",
+      ["seriesData": seriesData, "classLabels": classLabels, "classes": classes]
+    );
+  }
+
+  if (ops.contains('zip')) {
+    "rm -fR ./build/${seriesData.normalized_name}/docs".execute().waitFor();
+    "mkdir -p ./build/${seriesData.normalized_name}/docs".execute().waitFor();
+    for (c in classes) {
+      if (c.handout_file) {
+        if (c.handout_file instanceof Collection) {
+          for (def i=0; i<c.handout_file.size; i++) {
+            copyDoc(seriesData.normalized_name, c.handout_file[i], c.new_handout_file[i]);
+          }
+        } else {
+            copyDoc(seriesData.normalized_name, c.handout_file, c.new_handout_file);
+        }
+      }
+    }
+    /*
+    proc("tar -czf ./build/${seriesData.normalized_name}/${seriesData.normalized_name}-all.zip "+
+      "-C ./build/${seriesData.normalized_name} audio docs")
+    proc("tar -czf ./build/${seriesData.normalized_name}/${seriesData.normalized_name}-docs-only.zip "+
+      "-C ./build/${seriesData.normalized_name} docs")
+    */
+    proc("zip -rj ./build/${seriesData.normalized_name}/${seriesData.normalized_name}-docs.zip "+
+      "./build/${seriesData.normalized_name}/docs")
+    proc("zip -rj ./build/${seriesData.normalized_name}/${seriesData.normalized_name}-audio.zip "+
+      "./build/${seriesData.normalized_name}/audio")
+  }
+
+  if (ops.contains('s3-publish')) {
+    // put audio back to s3
+    // put zips up
+    // make files public
+  }
+
+}
+
+def String proc(def cmd) {
+  println cmd;
+  def proc = cmd.execute();
+  proc.waitFor();
+  println "return code: ${proc.exitValue()}"
+  println "stderr: ${proc.err.text}"
+  String outStr = proc.in.text
+  println "stdout: ${outStr}"
+  return outStr
+}
+
+def getNewHandoutFileName(id, ht, hf) {
+  def m = hf =~ /\.(.*)/
+  def ext = m[0][1];
+  ht = ht.replaceAll(' ', '_');
+  return "${id}-${ht}.${ext}"
+}
+
+def copyDoc(nn, hf, nhf) {
+  proc("cp ./orig/${nn}/docs/${hf} ./build/${nn}/docs/${nhf}")
+}
+
+def runVelocity(def templateFile, def outputFile, def data) {
+  def writer = new BufferedWriter(new FileWriter(outputFile))
+  Template template = Velocity.getTemplate(templateFile);
+  def context = new VelocityContext();
+  for (e in data) {
+    println "${e.key}: ${e.value}"
+    context.put(e.key, e.value);
+    
+  }
   template.merge(context, writer);
-
   writer.close();
-
-  println '3'
 }
