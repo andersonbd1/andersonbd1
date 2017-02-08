f784423edb05ea052225fbf7a5a44d6f582d3d78
Ben Anderson <benanderson@benanderson.us>
2017-02-08 16:10:38 -0500

    fan broke.

--- htdocs/vids/vid.php
+++ htdocs/vids/vid.php
@@ -4,9 +4,14 @@
   //var url = 'http://www.youtube.com/' + empty($_REQUEST['type']) ? 'v' : $_REQUEST['type'] +'/'+ $_REQUEST['id'] +'?rel=1&color1=0x2b405b&color2=0x6b8ab6&border=1&fs=1'
   $type = "v";
   if (!empty($_REQUEST['type'])) {
+    /*
+    if ($_REQUEST['type'] == 'p') {
+      $type = 'embed/'
+    }
+     */
     $type = $_REQUEST['type'];
   }
-  $url = "http://www.youtube.com/".$type."/". $_REQUEST['id']."?enablejsapi=1&version=3&playerapiid=ytplayer&autoplay=1&loop=1&rel=0&fs=1&modestbranding=1&iv_load_policy=3&playlist=".$_REQUEST['id'];
+  $url = "http://www.youtube.com/".$type."/".$_REQUEST['id']."?enablejsapi=1&version=3&playerapiid=ytplayer&autoplay=1&loop=1&rel=0&fs=1&modestbranding=1&iv_load_policy=3&list=".$_REQUEST['id'];
 
   if (!empty($_REQUEST['start'])) {
     $url = $url . "&start=" . $_REQUEST['start'];
@@ -15,7 +20,25 @@
     $url = $url . "&end=" . $_REQUEST['end'];
   }
 ?>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/jjn3fBTvBjY?list=PLFyz_VaHKmUll1kCZN4sCfbTeLR12dyqq&loop=1&fs=1&autoplay=1" frameborder="0" allowfullscreen></iframe>
+<!--
+<iframe width="560" height="315" src="https://www.youtube.com/embed/jjn3fBTvBjY?list=PLFyz_VaHKmUll1kCZN4sCfbTeLR12dyqq" frameborder="0" allowfullscreen></iframe>
+  <embed src="http://www.youtube.com/embed/jjn3fBTvBjY?enablejsapi=1&version=3&playerapiid=ytplayer&autoplay=1&loop=1&rel=0&fs=1&modestbranding=1&iv_load_policy=3&list=PLFyz_VaHKmUll1kCZN4sCfbTeLR12dyqq"
+-->
 
+<object width="425" height="355">
+  <param name="movie" value="<?= $url ?>"</param>
+  <param name="allowFullScreen" value="true"></param>
+  <param name="allowScriptAccess" value="always"></param>
+  <embed src="https://www.youtube.com/embed/jjn3fBTvBjY?list=PLFyz_VaHKmUll1kCZN4sCfbTeLR12dyqq"
+    type="application/x-shockwave-flash"
+    allowscriptaccess="always"
+    width="425" height="355" 
+    allowfullscreen="true"></embed>
+</object>
+
+<?php
+/*
 <object width="425" height="355">
   <param name="movie" value="<?= $url ?>"</param>
   <param name="allowFullScreen" value="true"></param>
@@ -26,9 +49,6 @@
     width="425" height="355" 
     allowfullscreen="true"></embed>
 </object>
-
-<?php
-/*
 <iframe id="ytplayer" type="text/html" width="640" height="390"
   src="http://www.youtube.com/<?= empty($_REQUEST['type']) ? 'v' : $_REQUEST['type'] ?>/<?= $_REQUEST['id'] ?>?loop=1&fs=1&autoplay=1"
   frameborder="0"/>
