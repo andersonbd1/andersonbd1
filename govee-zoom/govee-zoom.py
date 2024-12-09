b840959e4488f009087751fea8daaf9002d2af7b
Ben Anderson <benanderson@benanderson.us>
2024-12-09 08:42:02 -0500

    make it warmer

--- govee-zoom/govee-zoom.py
+++ govee-zoom/govee-zoom.py
@@ -3,14 +3,24 @@ import subprocess
 from time import sleep
 
 url = 'https://developer-api.govee.com/v1/devices/control'
-
 headers = {
     'Govee-API-Key': 'e1ec4b9b-6387-4ed0-8ec7-53cb914f8625',
     'accept': 'application/json',
     'content-type': 'application/json'
 }
+device_data = {
+    "device": "22:F4:D0:C9:07:00:23:A8",
+    "model": "H6008",
+}
+
+def send_request(data):
+    session = requests.Session()
+    session.headers.clear()  # Clear all default headers
+    response = session.put(url, headers=headers, json=data)
+    return response
 
 def change_color(is_busy: bool):
+    print(f"is_busy: {is_busy}")
     color_cmd = {
         "cmd": {
             "name": "color",
@@ -34,32 +44,23 @@ def change_color(is_busy: bool):
             "value": 4500 if is_busy else 3000
         }
     }
-    data = {
-        "device": "22:F4:D0:C9:07:00:23:A8",
-        "model": "H6008",
-    }
 
-    session = requests.Session()
-    session.headers.clear()  # Clear all default headers
+    for cmd in [color_cmd, brightness_cmd, colorTem_cmd]:
+        if is_busy and cmd == colorTem_cmd:
+            continue
+        data = device_data | cmd
+        print(f"data: {data}")
+        response = send_request(data)
+        print(f"Status Code: {response.status_code}")
+        print(f"Response: {response.text}")
 
-    """
-    data.pop("cmd")
-    response = requests.get('https://developer-api.govee.com/v1/devices', headers=headers)
-    """
-    response = session.put(url, headers=headers, json=data | color_cmd)
-    response = session.put(url, headers=headers, json=data | brightness_cmd)
-    response = session.put(url, headers=headers, json=data | colorTem_cmd)
-    response = requests.get('https://developer-api.govee.com/v1/devices/state', params=data, headers=headers)
-
-    print(f"Status Code: {response.status_code}")
-    print(f"Response: {response.text}")
+def check_zoom_activity():
+    cmd = "lsof -i 4UDP | grep zoom | awk 'END{print NR}'"
+    return subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
 
 prev_output = 0
-cmd = "lsof -i 4UDP | grep zoom | awk 'END{print NR}'"
-
-while (True):
-    output = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout
-
+while True:
+    output = check_zoom_activity()
     if prev_output != output:
         prev_output = output
         change_color(int(output) > 1)
